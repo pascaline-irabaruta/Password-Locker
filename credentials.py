341be05dbf0723d3e936106829f2045aa2211e23
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-15 13:46:08 +0200

    created a function to search for a credential

--- credentials.py
+++ credentials.py
@@ -24,3 +24,23 @@ class Credentials:
         funtion that deletes a credential
         '''
         Credentials.credential_list.remove(self)
+    @classmethod
+    def find_by_app_name(cls,app_name):
+        '''
+        function that takes an application name and return credential that matches it
+        helps user to search for a specific credential
+        '''
+        for credential in cls.credential_list:
+            '''
+            a for loop to loop through the list and return the credential that matches the app name
+            '''
+            if credential.app_name == app_name:
+                return credential
+        # for user in User.user_list:
+        #     '''
+        #     a for loop to loop through the list and check if the credential exists
+        #     '''
+        #     if user.app_name == app_name:
+        #         present_user = user.app_name
+        #         return present_user
+        # return "credential does not exist"
