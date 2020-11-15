e061b9b9f8c77b81ddb26e4c0833f966e3f97196
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-15 13:39:15 +0200

    created a function that hold credential properties

--- credentials.py
+++ credentials.py
@@ -5,3 +5,11 @@ class Credentials:
     Class that generates added credentials
     '''
     credential_list = []
+    def __init__(self,app_name , username, password):
+
+        '''
+        function that generate properties for our objects
+        '''
+        self.app_name = app_name
+        self.username = username
+        self.password = password
