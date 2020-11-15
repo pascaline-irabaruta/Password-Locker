0df5dd1a73e0c073d777a63d3cc187db9adca22c
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-15 13:55:57 +0200

    created a function to create a user

--- run.py
+++ run.py
@@ -5,3 +5,10 @@ import pyperclip
 import string
 import random
 from pyfiglet import Figlet
+
+def create_user(f_name,l_name,u_name,psswd):
+    '''
+    A funtcion to create a new user.
+    '''
+    new_user = User(f_name, l_name,u_name, psswd)
+    return new_user
