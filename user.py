5bab111b4e02ddd88655a4a93d79f3af2e4f7361
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-15 13:30:14 +0200

    created a function to define user properties

--- user.py
+++ user.py
@@ -3,3 +3,11 @@ class User():
     A class that prompts user to signup and login to generate passwords
     '''
     user_list = []
+    def __init__(self,f_name,l_name,u_name,psswd):
+        '''
+        a function to define user proprties
+        '''
+        self.f_name = f_name
+        self.l_name = l_name
+        self.u_name = u_name
+        self.psswd = psswd
